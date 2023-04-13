3ea759a8a42b3ef01dc9732e95889336b04607de
“Edward <eminaya49@gmail.com>
2023-04-13 12:02:31 -0500

    finished set 2

--- 2_select_from_world.sql
+++ 2_select_from_world.sql
@@ -21,3 +21,25 @@ SELECT name, population, area FROM world WHERE area > 3000000 OR population > 25
 
 -- 8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area
 SELECT name, population, area FROM world WHERE (area > 3000000 AND population <= 250000000) OR (area <= 3000000 AND population > 250000000);
+
+-- 9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+SELECT name, ROUND(population/1000000.00, 2), ROUND(gdp/1000000000.00, 2) FROM world WHERE continent = 'South America'
+
+-- 10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+SELECT name, ROUND(gdp/population, -3)  FROM world WHERE gdp >= 1000000000000
+
+-- 11. Show the name and capital where the name and the capital have the same number of characters.
+SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital)
+
+-- 12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+SELECT name, capital FROM world WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+
+-- 13. Find the country that has all the vowels and no spaces in its name.
+SELECT name
+FROM world
+WHERE name NOT LIKE '% %' -- Excludes countries with spaces in the name
+AND name LIKE '%a%' -- Must contain 'a'
+AND name LIKE '%e%' -- Must contain 'e'
+AND name LIKE '%i%' -- Must contain 'i'
+AND name LIKE '%o%' -- Must contain 'o'
+AND name LIKE '%u%'; -- Must contain 'u'
